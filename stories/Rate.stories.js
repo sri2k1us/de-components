2e9d80a3f27703e3517b4daa54e6adf253032be2
Sriram Srinivasan <sriram@cyverse.org>
2019-03-15 11:12:58 -0700

    Add rating component.

---
+++ stories/Rate.stories.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Rate from "../src/components/rating/Rate";
+
+
+class  RatingTest extends  Component{
+
+    render() {
+        return(
+          <Rate label="Rating" value={4.5} />
+        );
+    }
+}
+
+export default  RatingTest;
