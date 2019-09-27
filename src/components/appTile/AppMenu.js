bf5db2a5fb38cf412fa651cad9f74b60ce9aedcf
Sriram Srinivasan <sriram@cyverse.org>
2019-09-27 08:18:34 -0400

    Add AppMenu component.

---
+++ src/components/appTile/AppMenu.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { IconButton, Menu, MenuItem, makeStyles } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import withI18N, { getMessage } from "../../util/I18NWrapper";
+import { injectIntl } from "react-intl";
+import InfoIcon from "@material-ui/icons/InfoOutlined";
+import UnFavoriteIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import CommentsIcon from "@material-ui/icons/CommentOutlined";
+import palette from "../../util/CyVersePalette";
+import intlData from "./messages";
+
+/**
+ *
+ * @author sriram
+ *
+ * Menu items for Apps Tile and table view
+ *
+ */
+const useStyles = makeStyles((theme) => ({
+    menuItem: {
+        padding: 5,
+    },
+    toolbarItemColor: {
+        color: palette.darkBlue,
+    },
+}));
+
+function AppMenu(props) {
+    const {
+        onAppInfoClick,
+        onCommentsClick,
+        onFavoriteClick,
+        baseDebugId,
+        isExternal,
+        isFavorite,
+    } = props;
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClose = () => setAnchorEl(null);
+    const classes = useStyles();
+    return (
+        <div>
+            <IconButton
+                id={baseDebugId}
+                aria-label="More"
+                aria-owns={open ? "long-menu" : null}
+                aria-haspopup="true"
+                onClick={(event) => setAnchorEl(event.currentTarget)}
+            >
+                <MoreVertIcon />
+            </IconButton>
+            <Menu
+                id={baseDebugId + ".menu"}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+            >
+                <MenuItem
+                    disabled={false}
+                    className={classes.menuItem}
+                    data-disabled={false}
+                    onClick={() => {
+                        onAppInfoClick();
+                        handleClose();
+                    }}
+                >
+                    <InfoIcon className={classes.toolbarItemColor} />
+                    {getMessage("appInfo")}
+                </MenuItem>
+                {!isFavorite && !isExternal && (
+                    <MenuItem
+                        disabled={false}
+                        className={classes.menuItem}
+                        data-disabled={false}
+                        onClick={() => {
+                            onFavoriteClick();
+                            handleClose();
+                        }}
+                    >
+                        <FavoriteIcon className={classes.toolbarItemColor} />
+                        {getMessage("addToFavorites")}
+                    </MenuItem>
+                )}
+                {isFavorite && !isExternal && (
+                    <MenuItem
+                        disabled={false}
+                        className={classes.menuItem}
+                        data-disabled={false}
+                        onClick={() => {
+                            onFavoriteClick();
+                            handleClose();
+                        }}
+                    >
+                        <UnFavoriteIcon className={classes.toolbarItemColor} />
+                        {getMessage("removeFromFavorites")}
+                    </MenuItem>
+                )}
+                {!isExternal && (
+                    <MenuItem
+                        disabled={false}
+                        className={classes.menuItem}
+                        data-disabled={false}
+                        onClick={() => {
+                            onCommentsClick();
+                            handleClose();
+                        }}
+                    >
+                        <CommentsIcon className={classes.toolbarItemColor} />
+                        {getMessage("comments")}
+                    </MenuItem>
+                )}
+                {isExternal && (
+                    <React.Fragment>
+                        <MenuItem
+                            disabled={false}
+                            className={classes.menuItem}
+                            data-disabled={false}
+                            onClick={handleClose}
+                        >
+                            {getMessage("favoriteNotSupported")}
+                        </MenuItem>
+                        <MenuItem
+                            disabled={false}
+                            className={classes.menuItem}
+                            data-disabled={false}
+                            onClick={handleClose}
+                        >
+                            {getMessage("commentsNotSupported")}
+                        </MenuItem>
+                    </React.Fragment>
+                )}
+            </Menu>
+        </div>
+    );
+}
+
+export default withI18N(injectIntl(AppMenu), intlData);
--- src/components/appTile/messages.js
+++ src/components/appTile/messages.js
@@ -6,6 +6,12 @@ var intlData = {
         betaAppTooltip: "This app is in beta.",
         disabledAppTooltip: "This app is disabled.",
         useAppTooltip: "Use this app.",
+        addToFavorites: "Add To Favorites",
+        removeFromFavorites: "Remove From Favorites",
+        appInfo: "App Info",
+        comments: "Comments",
+        favoriteNotSupported: "Favorites not supported for external apps!",
+        commentsNotSupported: "Comments not supported for external apps!",
     },
 };
 
