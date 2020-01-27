ba80a71a7f81b73e8d584c86537485377340c4a9
Ashley Ramsey <aramsey@cyverse.org>
2020-01-27 14:35:38 -0700

    Refactor DotMenu story into Component Story Format

--- stories/DotMenu.stories.js
+++ stories/DotMenu.stories.js
@@ -1,48 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 import { MenuItem } from "@material-ui/core";
-import { storiesOf } from "@storybook/react";
 
 import DotMenu from "../src/components/dotMenu/DotMenu";
 
-export class DotMenuTest extends Component {
-    render() {
-        return (
-            <DotMenu
-                baseId="sampleDotMenu"
-                render={(onClose) => [
-                    <MenuItem
-                        key={1}
-                        onClick={() => {
-                            onClose();
-                            console.log("Clicked Item 1");
-                        }}
-                    >
-                        Item 1
-                    </MenuItem>,
-                    <MenuItem
-                        key={2}
-                        onClick={() => {
-                            onClose();
-                            console.log("Clicked Item 2");
-                        }}
-                    >
-                        Item 2
-                    </MenuItem>,
-                    <MenuItem
-                        key={3}
-                        onClick={() => {
-                            onClose();
-                            console.log("Clicked Item 3");
-                        }}
-                    >
-                        Item 3
-                    </MenuItem>,
-                ]}
-            />
-        );
-    }
-}
+export const DotMenuTest = () => {
+    return (
+        <DotMenu
+            baseId="sampleDotMenu"
+            render={(onClose) => [
+                <MenuItem
+                    key={1}
+                    onClick={() => {
+                        onClose();
+                        console.log("Clicked Item 1");
+                    }}
+                >
+                    Item 1
+                </MenuItem>,
+                <MenuItem
+                    key={2}
+                    onClick={() => {
+                        onClose();
+                        console.log("Clicked Item 2");
+                    }}
+                >
+                    Item 2
+                </MenuItem>,
+                <MenuItem
+                    key={3}
+                    onClick={() => {
+                        onClose();
+                        console.log("Clicked Item 3");
+                    }}
+                >
+                    Item 3
+                </MenuItem>,
+            ]}
+        />
+    );
+};
 
-storiesOf("DotMenu", module).add("with sample menu items", () => (
-    <DotMenuTest />
-));
+export default { title: "DotMenu" };
