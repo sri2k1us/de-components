865b8a867a138265f87085e9d2fb3c586ad9b066
Ian McEwen <mian@cyverse.org>
2020-05-18 16:43:22 -0700

    use more compact export definitions

--- src/util/FormField.js
+++ src/util/FormField.js
@@ -1,28 +1,20 @@
 /**
  * @author psarando
  */
-import FormCheckbox from "./forms/FormCheckbox";
-import FormCheckboxStringValue from "./forms/FormCheckboxStringValue";
-import FormCheckboxTableCell from "./forms/FormCheckboxTableCell";
-import FormIntegerField from "./forms/FormIntegerField";
-import FormMultilineTextField from "./forms/FormMultilineTextField";
-import FormNumberField from "./forms/FormNumberField";
-import FormSearchField from "./forms/FormSearchField";
-import FormSelectField from "./forms/FormSelectField";
-import FormTextField from "./forms/FormTextField";
-import FormTimestampField from "./forms/FormTimestampField";
-import getFormError from "./forms/getFormError";
-
+export { default as FormCheckbox } from "./forms/FormCheckbox";
 export {
-    FormCheckbox,
-    FormCheckboxStringValue,
-    FormCheckboxTableCell,
-    FormIntegerField,
-    FormMultilineTextField,
-    FormNumberField,
-    FormSearchField,
-    FormSelectField,
-    FormTextField,
-    FormTimestampField,
-    getFormError,
-};
+    default as FormCheckboxStringValue,
+} from "./forms/FormCheckboxStringValue";
+export {
+    default as FormCheckboxTableCell,
+} from "./forms/FormCheckboxTableCell";
+export { default as FormIntegerField } from "./forms/FormIntegerField";
+export {
+    default as FormMultilineTextField,
+} from "./forms/FormMultilineTextField";
+export { default as FormNumberField } from "./forms/FormNumberField";
+export { default as FormSearchField } from "./forms/FormSearchField";
+export { default as FormSelectField } from "./forms/FormSelectField";
+export { default as FormTextField } from "./forms/FormTextField";
+export { default as FormTimestampField } from "./forms/FormTimestampField";
+export { default as getFormError } from "./forms/getFormError";
